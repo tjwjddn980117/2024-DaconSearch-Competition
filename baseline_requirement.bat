704c3c8a53d153301065fa9f49b28f1ebe87ad7e
tjwjddn980117 <tjwjddn15584@naver.com>
2024-07-30 09:39:44 +0900

    requirement env of baseline code

---
+++ baseline_requirement.bat
@@ -0,0 +1,36 @@
+rem you should change the root with your own environment path root.
+rem and you could change the ENV_NAME with your one vitual environment.
+set root=C:\Users\Seo\anaconda3
+set ENV_NAME=Search_Baseline
+
+if not exist "%root%" (
+    echo check the root please: %root%
+    pause
+    exit
+)
+
+call %root%\Scripts\activate.bat %root%
+
+echo make the virtual environment '%ENV_NAME%'
+call conda create -y -n %ENV_NAME% python=3.10
+
+echo enter the virtual environment.
+call conda activate %ENV_NAME%
+
+echo start downloading environment for %ENV_NAME%.
+call conda install -y pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+call conda install -y conda-forge::tqdm
+call pip install accelerate
+callpip install -i https://pypi.org/simple/ bitsandbytes
+call pip install transformers[torch] -U
+
+call pip install datasets
+call pip install langchain
+call pip install langchain_community
+call pip install PyMuPDF
+call pip install sentence-transformers
+call pip install faiss-cpu
+
+call conda deactivate
+
+echo complete. 
